afc613a291944f5d965dcfb85f7081ba51f4e1c8
Abdul Jaleel Malik <abduljaleelmalik@Lions-Mac.local>
2012-03-26 15:23:23 +0400

    Initial Commit

---
+++ NozzleRush/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  NozzleRush
+//
+//  Created by Abdul Jaleel Malik on 26/03/2012.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
